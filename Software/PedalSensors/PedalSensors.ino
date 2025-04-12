4b509892d0809f468106a637abe5f0000e4f56f4
andrewsantella <andrewsantella1@gmail.com>
2025-04-12 19:09:49 -0400

    Add offset transformation for pressure sensors (see desc)
    Not sure why, but the analog reading value is consistently slightly less than it should be. Originally I though something was wrong with the voltage divider or software, but it appears to be an issue internal to the ESP32/ADC. When I measured 0.50V from the sensor on the analog pin, the ESP32 would report an analog reading that corresponded to an input voltage of ~0.32. This result carried through for 1V, 2V, 3V, etc. It appears to be linearly incorrect, so I added an offset function that ensures the pin voltage is accurately reconstructed after ADC processing

--- Software/PedalSensors/PedalSensors.ino
+++ Software/PedalSensors/PedalSensors.ino
@@ -7,12 +7,12 @@ const int frontBrakePressurePin = 33;
 const int rearBrakePressurePin = 34;
 const int gasPedalPin = 35;
 
-const float frontBrakeR1 = 2180;
-const float frontBrakeR2 = 969;
-const float rearBrakeR1 = 2164;
-const float rearBrakeR2 = 980;
-const float gasPedalR1 = 2180;
-const float gasPedalR2 = 980;
+const float frontBrakeR1 = 969;
+const float frontBrakeR2 = 2180;
+const float rearBrakeR1 = 980;
+const float rearBrakeR2 = 2164;
+const float gasPedalR1 = 980;
+const float gasPedalR2 = 2180;
 
 int frontBrakeAnalogValue = 0;
 int rearBrakeAnalogValue = 0;
@@ -23,6 +23,7 @@ float rearBrakeVoltage = 0;
 int gasPedalAnalogValue = 0;
 
 void setup() {
+
 
   // Initialize CAN Functionality
   setupCAN(PEDALS);
@@ -37,12 +38,44 @@ void setup() {
 
 void loop() {
 
+  /*
+  * For some reason, the analog reads on this system are not correct. The pin was given the following
+  * voltages directly, and the following analog readings were calculated back to voltages based on
+  * the voltage divider values. Here's what was found:
+  *
+  * GIVEN:    REPORTED:
+  * 0.25      0.08
+  * 0.5       0.32
+  * 0.75      0.56
+  * 1.00      0.81
+  * 1.50      1.31
+  * 2.00      1.80
+  * 2.50      2.30
+  * 3.00      2.79
+  * 3.50      3.30
+  * 4.00      3.90
+  * 4.25      4.30
+  * 4.50      4.76
+  * 4.75      4.79
+  * 5.00      4.79 
+  *
+  *
+  * Up until 4V, this data is nearly perfectly linear. 4V corresponds to roughly 1300 PSI, and we don't expect
+  * to see anything quite that high, so we can just rock with a linear transformation for these analog reads.
+  * The calculated equation from the spreadsheet was Output Voltage = 0.993*In + 0.196V with an R^2 of 0.999.
+  *
+  */
+
   frontBrakeAnalogValue = analogRead(frontBrakePressurePin);
   frontBrakeVoltage = (float)frontBrakeAnalogValue / 4095.0 * 3.3;
-  frontBrakeVoltage = frontBrakeVoltage  *  (frontBrakeR1+frontBrakeR2) / frontBrakeR2;
+  frontBrakeVoltage = frontBrakeVoltage * (frontBrakeR1 + frontBrakeR2) / frontBrakeR2;
+  frontBrakeVoltage = 0.993*frontBrakeVoltage + 0.196; // See comment above about this linear transformation
   frontBrakePressure = floatMap(frontBrakeVoltage, 0.5, 4.5, 0, 1500);
   Serial.print("frontBrakeAnalogValue:");
   Serial.print(frontBrakeAnalogValue);
+  Serial.print(", ");
+  Serial.print("frontBrakeVoltage:");
+  Serial.print(frontBrakeVoltage);
   Serial.print(", ");
   Serial.print("frontBrakePressure:");
   Serial.print(frontBrakePressure);
@@ -50,14 +83,18 @@ void loop() {
 
   rearBrakeAnalogValue = analogRead(rearBrakePressurePin);
   rearBrakeVoltage = (float)rearBrakeAnalogValue / 4095.0 * 3.3;
-  rearBrakeVoltage = rearBrakeVoltage  *  (rearBrakeR1+rearBrakeR2) / rearBrakeR2;
+  rearBrakeVoltage = rearBrakeVoltage * (rearBrakeR1 + rearBrakeR2) / rearBrakeR2;
+  rearBrakeVoltage = rearBrakeVoltage * 0.993 + 0.196;  // See comment above about this linear transformation
   rearBrakePressure = floatMap(rearBrakeVoltage, 0.5, 4.5, 0, 1500);
   Serial.print("rearBrakeAnalogValue:");
   Serial.print(rearBrakeAnalogValue);
   Serial.print(", ");
+  Serial.print("rearBrakeVoltage:");
+  Serial.print(rearBrakeVoltage);
+  Serial.print(", ");
   Serial.print("rearBrakePressure:");
   Serial.print(rearBrakePressure);
-    Serial.print(", ");
+  Serial.print(", ");
 
 
 
